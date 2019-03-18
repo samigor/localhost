cf9741ea62d1ad6f82b55f7adae2d9037bf664ba
samigor <47845766+samigor@users.noreply.github.com>
2019-03-18 17:24:11 +0200

    1

---
+++ test/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<head>
+	<title>Тест</title>
+</head>
+<body>
+
+<table width="50%" border="1" align='center'>
+
+<tr>
+	<td align="center">
+		<!-- Верхняя часть страницы -->
+		<table width="100%">
+			<tr>
+				<td align="center">
+					<h1></h1>
+					<p><a href='/test/index.php'>Прервать тест и вернуться на сайт</a></p>
+				</td>
+			</tr>
+		</table>
+		<!-- Верхняя часть страницы -->
+	</td>
+</tr>
+
+<tr>
+	<td>
+		<!-- Область основного контента -->		
+		<!-- Область основного контента -->
+	</td>
+</tr>
+</table>
+
+</body>
+</html>
---
+++ test/result.php
@@ -0,0 +1,7 @@
+<table width="100%">
+	<tr>
+		<td align="left">
+		<p>Ваш результат <?php echo $result; ?> из 30</p>
+		</td>
+	</tr>
+</table>
